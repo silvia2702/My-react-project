32ddb321269bc80ca1a30e522f323583bb1eaec6
Isabella09 <isagiraffe79@gmail.com>
2022-01-28 14:28:40 -0500

    fixed video

--- my/src/components/HeroSection/HeroSection.js
+++ my/src/components/HeroSection/HeroSection.js
@@ -4,9 +4,9 @@ import './HeroSection.css'
 function HeroSection() {
   return (
    <div className='hero-container'>
-      {<video Classname="cover-video" src='/videos/intro.mp4' autoPlay loop muted/>}
+      <video className="cover-video" src='/videos/intro.mp4' autoPlay loop muted/>
       <h1>Welcome</h1>
-      <p>Welcome to my website where I talk about the shows I've watched and rate them.</p>
+      <p>Welcome to my website where I talk about the shows I've watched and rate them. Hope you enjoy.</p>
   </div>
   )
   
