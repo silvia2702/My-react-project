6506c9390e446acbeac80f25790872ac498c90db
Isabella09 <isagiraffe79@gmail.com>
2022-03-08 13:34:16 -0500

    added shows

--- my/src/components/MovieReviewDisplay/movieReviewDisplay.js
+++ my/src/components/MovieReviewDisplay/movieReviewDisplay.js
@@ -2,18 +2,32 @@ import React from 'react'
 import ProjectCard from '../ProjectCard/ProjectCard'
 import './movieReviewDisplay.css'
 
-const prj = [{
+const prj = [
+  {
     title: "Attack on Titan",
     entry: "Eren Yeager",
     src: 'https://images.unsplash.com/photo-1531558506007-fe311a2f4729?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2t5c2NyYXBlcnxlbnwwfHwwfHw%3D&w=1000&q=80',
     key: 1
+  },
+  
+  {
+    title: "Haikyu",
+    entry: "Tsuki",
+    src: 'https://images.unsplash.com/photo-1531558506007-fe311a2f4729?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2t5c2NyYXBlcnxlbnwwfHwwfHw%3D&w=1000&q=80',
+    key: 2
+  },
 
-},
+  {
+    title: "Naruto",
+    entry: "What a drag",
+    src: 'https://images.unsplash.com/photo-1531558506007-fe311a2f4729?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2t5c2NyYXBlcnxlbnwwfHwwfHw%3D&w=1000&q=80',
+    key:3
+  },
 
 ]
 function movieReviewDisplay() {
   return (
-    <div className = 'movie-review'>movieReviewDisplay
+    <div className = 'movie-review'>
      <img className = 'movie-review-background' alt = 'skyscraper background' src = 'https://images.unsplash.com/photo-1531558506007-fe311a2f4729?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2t5c2NyYXBlcnxlbnwwfHwwfHw%3D&w=1000&q=80' >
       </img>
       {prj.map((item) => <ProjectCard props={item} key={item.key}/>)} 
