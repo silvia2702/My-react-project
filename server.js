15e04cf8bc69206560147506ba4f40282f6bc016
Isabella09 <isagiraffe79@gmail.com>
2022-03-22 14:22:07 -0400

    crashed it but its ok

--- server.js
+++ server.js
@@ -2,11 +2,30 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const fs = require("fs")
+const path = require("path");
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+const POST_DIR = "./my/public/posts"
+
+// app.use(express.static(path.join(__dirname, "/public")))
+
+app.get("/posts", async (req, res) => {
+    const postDir = "./public/posts"
+    if(!fs.existsSync("./public/posts")) fs.mkdirSync(postDir)
+
+    const postsNames = fs.readdirSync(postDir);
+    const posts = [];
+
+    postsNames.forEach(postName => {
+        posts.push(JSON.parse(fs.readFileSync(postDir + "/" + postName)))
+    })
+    
+    console.log(postsNames);
+
+    res.send(posts);
+})
 
 app.post('/' , (req,res) => {
-    const POST_DIR = "./my/public/posts"
     if(!fs.existsSync(POST_DIR)) fs.mkdirSync(POST_DIR)
 
     fs.writeFileSync(POST_DIR + "/" + req.body.title + ".json", JSON.stringify(req.body, null, 2))
